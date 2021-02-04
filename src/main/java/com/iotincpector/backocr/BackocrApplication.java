77ecc44c31b116fe5bfe4df8c70adb1fe2d60579
eni-kliszki <kliszkieniko@gmail.com>
2021-02-04 08:53:35 +0100

    first commit

---
+++ src/main/java/com/iotincpector/backocr/BackocrApplication.java
@@ -0,0 +1,13 @@
+package com.iotincpector.backocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackocrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackocrApplication.class, args);
+    }
+
+}
