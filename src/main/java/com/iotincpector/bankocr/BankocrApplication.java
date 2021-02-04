665159b4180765d3186f373079106f9de4071070
eni-kliszki <kliszkieniko@gmail.com>
2021-02-04 09:34:58 +0100

    add filereader and test it in main

--- src/main/java/com/iotincpector/bankocr/BankocrApplication.java
+++ src/main/java/com/iotincpector/bankocr/BankocrApplication.java
@@ -1,13 +1,25 @@
 package com.iotincpector.bankocr;
 
+import com.iotincpector.bankocr.controller.AccountController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BankocrApplication {
 
+    @Autowired
+    AccountController accountController;
+
     public static void main(String[] args) {
         SpringApplication.run(BankocrApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        accountController.consollog("src/main/java/com/iotincpector/bankocr/resource/accountText_us1.txt");
     }
 
 }
--- src/main/java/com/iotincpector/bankocr/controller/AccountController.java
+++ src/main/java/com/iotincpector/bankocr/controller/AccountController.java
@@ -1,7 +1,21 @@
 package com.iotincpector.bankocr.controller;
 
+import com.iotincpector.bankocr.model.ReadFromTxtFile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
 
 @RestController
 public class AccountController {
+    @Autowired
+    ReadFromTxtFile readFromTxtFile;
+
+    public void consollog(String path){
+        try {
+            System.out.println(readFromTxtFile.read(path));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
